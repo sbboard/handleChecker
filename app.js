0c37c25bb73f407fe736a9ec3aac301b74f5c424
sbboard <colin.buffum@gmail.com>
2019-11-26 22:32:24 -0700

    finished twitter API useage

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var Twit = require('twit')
 
+let bigArray = []
+const names = ["buffum","seanporter","buffumwad","colinbuffum"]
+
 var fs = require('fs'),
     path = require('path'),
     Twit = require('twit'),
@@ -7,11 +10,43 @@ var fs = require('fs'),
 
 var T = new Twit(config);
 
-T.get('users/lookup', { screen_name: 'buffum' },  function (err, data, response) {
-    if(data.hasOwnProperty('errors') && data.errors[0].code == 17){
-        console.log("doesn't exist")
+function checkStatus(name){
+    return new Promise(resolve => {
+        let status = ""
+        T.get('users/lookup', { screen_name: name },  function (err, data, response) {
+            if(data.hasOwnProperty('errors') && data.errors[0].code == 17){
+                status = "FREE"
+            }
+            else{                
+                status = "TAKEN"
+            }
+            bigArray.push({"name":name,"status":status})
+            resolve('resolved')
+        })
+    })
+}
+
+function sendEmail(){
+    return new Promise(resolve => {
+    console.log(bigArray)
+    resolve('resolved')
+    })
+}
+
+async function runScript(){
+    //get buffum status
+    //get sean status
+    //get controlled negative
+    //get controlled positive
+    //send email
+    for(let i = 0; i < names.length; i++){
+        await checkStatus(names[i])
     }
-    else{
-        console.log(data)
-    }
-})
+    await sendEmail()
+}
+
+runScript()
+
+setInterval(function(){
+    runScript()
+  }, 24*60*60*1000);
