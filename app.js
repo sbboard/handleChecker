57e25a052c876a5620fca7c0700aae67cf900b89
sbboard <colin.buffum@gmail.com>
2019-11-26 22:39:33 -0700

    wrote send email function

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var Twit = require('twit')
+const nodemailer = require("nodemailer")
 
 let bigArray = []
 const names = ["buffum","seanporter","buffumwad","colinbuffum"]
@@ -10,15 +11,24 @@ var fs = require('fs'),
 
 var T = new Twit(config);
 
+let transporter = nodemailer.createTransport({
+    name: 'gang-fight.com',
+    host: 'localhost',
+    port: 25,
+    tls:{
+        rejectUnauthorized: false
+    }
+});
+
 function checkStatus(name){
     return new Promise(resolve => {
         let status = ""
         T.get('users/lookup', { screen_name: name },  function (err, data, response) {
             if(data.hasOwnProperty('errors') && data.errors[0].code == 17){
-                status = "FREE"
+                status = true
             }
             else{                
-                status = "TAKEN"
+                status = false
             }
             bigArray.push({"name":name,"status":status})
             resolve('resolved')
@@ -28,17 +38,33 @@ function checkStatus(name){
 
 function sendEmail(){
     return new Promise(resolve => {
-    console.log(bigArray)
-    resolve('resolved')
+        let message = "<table style='border: 1px solid black; width: 100%'>"
+        let plainMsg = ""
+        for(j = 0; j<bigArray.length; j++){
+            message += `<tr><td style='border: 1px solid black'><b>${bigArray[i].name}</b></td>`
+            plainMsg += `${bigArray[i].name}: `
+            if(bigArray[i].status == true){
+                message += `<tr><td style='border: 1px solid black;color:green'><b>${bigArray[i].status}</b></td>`
+                plainMsg += `${bigArray[i].status}`
+            }
+            else{
+                message += `<tr><td style='border: 1px solid black;color:red'><b>${bigArray[i].status}</b></td>`
+                plainMsg += `${bigArray[i].status}`
+            }
+        }
+        message += "</table>"
+        transporter.sendMail({
+            from: '"Jolly Boy" <twitter@gang-fight.com>', // sender address
+            to: "colin.buffum@gmail.com", // list of receivers
+            subject: "Twitter Handle Update", // Subject line
+            text: plainMsg, // plain text body
+            html: message // html body
+        });
+        resolve('resolved')
     })
 }
 
 async function runScript(){
-    //get buffum status
-    //get sean status
-    //get controlled negative
-    //get controlled positive
-    //send email
     for(let i = 0; i < names.length; i++){
         await checkStatus(names[i])
     }
